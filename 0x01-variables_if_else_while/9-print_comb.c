1b7b3fe7eef3c5a85d8fd0c4eca09e0e0df03bac
binyamfeyissa <101417052+binyamfeyissa@users.noreply.github.com>
2022-04-08 16:23:07 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 {
-int k;
+	int numb;
 
-for (k = 48; k <= 57; k++)
-{
-putchar(k);
-if (k < 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	for (numb = '0' ; numb <= '9' ; numb++)
+	{
+		putchar(numb);
+
+		if (numb != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
